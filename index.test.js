8153744b1f24d776dc6ac4c35a3c5baf4c0dd40f
zacanger <zac@zacanger.com>
2020-03-22 12:03:44 -0600

    chore: switch to eslint

--- index.test.js
+++ index.test.js
@@ -1,8 +1,9 @@
+// eslint-disable-next-line node/no-unpublished-require
 const tape = require('tape')
 const isInstalled = require('.')
 
 tape.test('is-module-installed', (t) => {
-  t.true(isInstalled('standard'))
+  t.true(isInstalled('eslint'))
   t.false(isInstalled('lkjsfdlkjsdfllkjsldkjljdf'))
   t.end()
 })
